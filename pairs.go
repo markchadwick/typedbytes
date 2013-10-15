b2d3d194f6aaacd21106e7f1b807375a523324f6
Mark Chadwick <mark@vistarmedia.com>
2013-10-14 20:04:27 -0400

    Added Gomfile

--- pairs.go
+++ pairs.go
@@ -12,7 +12,7 @@ func NewPairReader(reader *Reader) *PairReader {
 	return &PairReader{reader}
 }
 
-func (pr *PairReader) Read() (k, v interface{}, err error) {
+func (pr *PairReader) Next() (k, v interface{}, err error) {
 	if k, err = pr.reader.Next(); err != nil {
 		return
 	}
@@ -44,6 +44,6 @@ func (pw *PairWriter) Write(k, v interface{}) (err error) {
 	return
 }
 
-func (pw *PairWriter) Close() {
-	pw.writer.Close()
+func (pw *PairWriter) Close() error {
+	return pw.writer.Close()
 }
